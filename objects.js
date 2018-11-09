30490105133c84821c8296dc43f6ad8f9b677532
Sergio Neves <serneves@hotmail.com>
2018-11-09 12:33:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
+function removeFromPlaylist(artistName) {
   delete playlist.artistName
   return playlist
 }

12eb72503b948fed5dd26454d9f66d6cd944df3a
Sergio Neves <serneves@hotmail.com>
2018-11-09 12:29:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = { artista : "musica" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-playlist [artistName] = songTitle
-return playlist
+  playlist [artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  playlist.delete(artistName)
+  return playlist
 }
